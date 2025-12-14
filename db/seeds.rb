49cb8a4499c9f4fe88d536942a6682761cc021a8
VladimirVarelaH <vladimir.varelah@gmail.com>
2025-12-13 22:10:51 -0300

    database connection updated and seeder added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+CATEGORIES = {
+  'Alimentación' => { icon: 'utensils', color: 'green' },
+  'Salud' => { icon: 'pill', color: 'red' },
+  'Transporte' => { icon: 'car', color: 'blue' },
+  'Vestuario' => { icon: 'shirt', color: 'purple' },
+  'Hogar' => { icon: 'home', color: 'orange' },
+  'Educación' => { icon: 'book', color: 'indigo' },
+  'Entretenimiento' => { icon: 'film', color: 'pink' },
+  'Servicios' => { icon: 'briefcase', color: 'cyan' },
+  'Otros' => { icon: 'shopping-bag', color: 'gray' }
+}
+
+puts "Seeding categories..."
+
+CATEGORIES.each do |name, attrs|
+  category = Category.find_or_initialize_by(name: name)
+  category.icon = attrs[:icon]
+  category.color = attrs[:color]
+  category.save!
+end
+
+puts "✔ Categorías creadas/actualizadas correctamente."
